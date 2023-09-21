c94dad2eba2d7a49379a0820d9d13f23034a7f5d
thgee <xogur25@naver.com>
2023-09-22 01:47:26 +0900

    FEAT: 로그인 페이지 구현 완료

--- interface/src/pages/Login.tsx
+++ interface/src/pages/Login.tsx
@@ -10,6 +10,12 @@ const Login: React.FC = () => {
         src={process.env.PUBLIC_URL + `/assets/인페 로고-03.svg`}
       />
       <LoginBox />
+
+      <div className="signup-wrapper">
+        <div className="signup-text">아이디 / 비밀번호를 잊으셨나요?</div>
+        <div className="bar"></div>
+        <div className="signup-text">회원가입</div>
+      </div>
     </div>
   );
 };
