2419b0f60dff143498af9e9562f40ce122b51754
bsu0404 <bsu0404@naver.com>
2023-11-09 01:05:13 +0900

    Feat: 원 이미지 추가

---
+++ interface/src/pages/Notice.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import NoticeBox from "../components/NoticeBox";
+
+const Notice: React.FC = () => {
+  return (
+    <div className="Notice">
+      <Header />
+      <NoticeBox />
+    </div>
+  );
+};
+
+export default Notice;
