783e2480358ed8ebfe057b723d431945e32dd5a7
thgee <102576089+thgee@users.noreply.github.com>
2023-11-10 14:49:36 +0900

    FEAT: 월 선택 박스 구현중

---
+++ interface/src/components/MonthButton.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+interface MonthButtonProps {
+  month: number;
+}
+
+const MonthButton: React.FC<MonthButtonProps> = ({ month }) => {
+  const [isSelect, setIsSelect] = useState(false);
+
+  return (
+    <div
+      className={`MonthButton ${isSelect}`}
+      onClick={() => {
+        setIsSelect(true);
+      }}
+    >
+      <div>{month}월</div>
+    </div>
+  );
+};
+// 근데 너무 어렵다ㅠ
+export default MonthButton;
--- interface/src/components/PlanBox.tsx
+++ interface/src/components/PlanBox.tsx
@@ -1,10 +1,11 @@
 import PlanCard from "./PlanCard";
-import VoteCard from "./VoteCard";
+import SelectMonthBox from "./SelectMonthBox";
 
 const PlanBox: React.FC = () => {
   return (
     <div className="PlanBox">
       <div className="notice-sub-title">일정 선택</div>
+      <SelectMonthBox />
 
       <PlanCard />
       <PlanCard />
